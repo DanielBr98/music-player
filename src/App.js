1a3ba941ec37b43273dd6b9d5d1418c4a0c0e6e1
DanielBr98 <danielbrasild10@hotmail.com>
2022-01-10 21:35:08 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import icon from './images/icon.png'
+import background from './images/background.jpg'
+import ReactJkMusicPlayer from 'react-jinke-music-player'
+import 'react-jinke-music-player/assets/index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import music01 from './musics/Cartoon - On _ On (feat. Daniel Levi) _NCS Release_.mp3'
+import music02 from './musics/Cartoon - Why We Lose (feat. Coleman Trapp) _NCS Release_.mp3'
+import music03 from './musics/DEAF KEV - Invincible _NCS Release_.mp3'
+import music04 from './musics/Different Heaven _ EH DE - My Heart _NCS Release_.mp3'
+import music05 from './musics/Disfigure - Blank _NCS Release_.mp3'
+import music06 from './musics/Electro-Light - Symbolism _NCS Release_.mp3'
+import music07 from './musics/Elektronomia - Sky High [NCS Release].mp3'
+import music08 from './musics/Janji - Heroes Tonight (feat. Johnning) _NCS Release_.mp3'
+import music09 from './musics/Warriyo - Mortals (feat. Laura Brehm) [NCS Release].mp3'
+import music10 from './musics/Tobu - Hope [NCS Release].mp3'
+
+class App extends Component {
+
+  render() {
+    const background_style = {
+      backgroundImage: `url(${background})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      width: '100vw',
+      height: '100vh'
+    }
+
+    const audioList = [
+      {
+        cover: icon,
+        musicSrc: music01,
+        name: 'On _ On (feat. Daniel Levi)',
+        singer: 'Cartoon'
+      },
+      {
+        cover: icon,
+        musicSrc: music02,
+        name: 'Why We Lose (feat. Coleman Trapp)',
+        singer: 'Cartoon'
+      },
+      {
+        cover: icon,
+        musicSrc: music03,
+        name: 'Invincible',
+        singer: 'DEAF KEV'
+      },
+      {
+        cover: icon,
+        musicSrc: music04,
+        name: 'My Heart',
+        singer: 'Different Heaven _ EH DE'
+      },
+      {
+        cover: icon,
+        musicSrc: music05,
+        name: 'Blank',
+        singer: 'Disfigure'
+      },
+      {
+        cover: icon,
+        musicSrc: music06,
+        name: 'Symbolism',
+        singer: 'Electro-Light'
+      },
+      {
+        cover: icon,
+        musicSrc: music07,
+        name: 'Sky High',
+        singer: 'Elektronomia'
+      },
+      {
+        cover: icon,
+        musicSrc: music08,
+        name: 'Heroes Tonight (feat. Johnning)',
+        singer: 'Janji'
+      },
+      {
+        cover: icon,
+        musicSrc: music09,
+        name: 'Mortals (feat. Laura Brehm)',
+        singer: 'Warriyo'
+      },
+      {
+        cover: icon,
+        musicSrc: music10,
+        name: 'Hope',
+        singer: 'Tobu'
+      },
+    ]
+
+    return (
+      <div style={background_style}>
+        <ReactJkMusicPlayer audioLists={audioList} autoPlay={false} toggleMode={false} mode="full" />
+      </div>
+    )
+  }
 }
 
 export default App;
